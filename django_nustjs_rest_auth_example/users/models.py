338886d44d16694f380fd8c6750025cefb160ccf
Vladimir Myshkovski <vladimirmyshkovski@gmail.com>
2019-05-21 20:16:16 +0400

    Removed get_absolute_url method from User model

--- django_nustjs_rest_auth_example/users/models.py
+++ django_nustjs_rest_auth_example/users/models.py
@@ -9,6 +9,3 @@ class User(AbstractUser):
     # First Name and Last Name do not cover name patterns
     # around the globe.
     name = CharField(_("Name of User"), blank=True, max_length=255)
-
-    def get_absolute_url(self):
-        return reverse("users:detail", kwargs={"username": self.username})
