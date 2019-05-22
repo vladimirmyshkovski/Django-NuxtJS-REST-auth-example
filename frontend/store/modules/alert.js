3c8d1a3f035cce9916a7c90332189b1d81062540
Vladimir Myshkovski <vladimirmyshkovski@gmail.com>
2019-05-22 13:46:20 +0400

    Added mutations to auth store module

--- frontend/store/modules/alert.js
+++ frontend/store/modules/alert.js
@@ -1,3 +1,5 @@
+import Cookies from 'js-cookie'
+
 const state = () => ({
   user: null,
   token: null,
@@ -20,7 +22,37 @@ const getters = {
   }
 }
 
+const mutations = {
+  setUser(state, payload) {
+    state.user = payload
+  },
+  setToken(state, payload) {
+    state.token = payload
+  },
+  setLoggedIn(state, payload) {
+    state.loggedIn = payload
+  },
+  setCookie(state, payload) {
+    let minutes
+    if ('date' in payload) {
+      minutes = payload.date
+    } else {
+      minutes = 30
+    }
+    const date = new Date()
+    date.setTime(date.getTime() + minutes * 60 * 1000)
+    Cookies.set(payload.key, payload.value, { expires: date })
+  },
+  clearCookie(state, payload) {
+    Cookies.remove(payload.key)
+  },
+  setErrors(state, payload) {
+    state.errors = payload
+  }
+}
+
 export default {
   state,
-  getters
+  getters,
+  mutations
 }
