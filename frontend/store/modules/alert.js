e5383917d3260d596105a68a6fea0cd8467d6e19
Vladimir Myshkovski <vladimirmyshkovski@gmail.com>
2019-05-22 13:44:35 +0400

    Added getters to auth store module

--- frontend/store/modules/alert.js
+++ frontend/store/modules/alert.js
@@ -5,6 +5,22 @@ const state = () => ({
   errors: {}
 })
 
+const getters = {
+  user(state) {
+    return state.user
+  },
+  token(state) {
+    return state.token
+  },
+  loggedIn(state) {
+    return state.loggedIn
+  },
+  errors(state) {
+    return state.errors
+  }
+}
+
 export default {
-  state
+  state,
+  getters
 }
